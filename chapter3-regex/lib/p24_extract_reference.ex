5fd3b5f6aac86723f93e815d90036239070f9f73
user.email <efg.river@gmail.com>
2019-03-02 19:58:54 +0900

    Chapter3 P24

--- chapter3-regex/lib/p24_extract_reference.ex
+++ chapter3-regex/lib/p24_extract_reference.ex
@@ -5,6 +5,7 @@ defmodule P24ExtractReference do
 
     WikiParser.search_by_country("jawiki-country.json", "イギリス")
     |> WikiParser.fetch_text_lines
-    |> Enum.each ( &( &1 |> IO.puts ))
+    |> Enum.filter( &( Regex.match?(~r/ファイル:(.+)/, &1) ) )
+    |> Enum.each( &( Regex.run(~r/ファイル:(.+)\|(.+)/, &1) |> Enum.at(1) |> IO.puts) )
   end
 end
